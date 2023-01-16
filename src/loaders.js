444ec850e9015cf8a7ae6ebe401f1e8400dcfd35
Symon <simon16143@gmail.com>
2023-01-16 12:04:03 -0500

    fixing image issues2

--- src/loaders.js
+++ src/loaders.js
@@ -1,5 +1,5 @@
 export const appLoader = async ( ) => {
-    const response = await fetch("http://zine-hosteleria-backend12012023.onrender.com")
+    const response = await fetch("https://zine-hosteleria-backend12012023.onrender.com")
     const data = await response.json()
     const result = data
     return result
