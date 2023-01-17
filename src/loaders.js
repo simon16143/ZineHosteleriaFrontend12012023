404c02fa1647e3724c2d84c3cbc8d0db6c07cbd4
Symon <simon16143@gmail.com>
2023-01-17 15:07:40 -0500

    working routes

--- src/loaders.js
+++ src/loaders.js
@@ -2,5 +2,6 @@ export const appLoader = async ( ) => {
     const response = await fetch("https://zine-hosteleria-backend12012023.onrender.com")
     const data = await response.json()
     const result = data
+    console.log(result)
     return result
 }
